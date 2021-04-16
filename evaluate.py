0babf7a238015f78fdde332d14aebc8cdc088149
willwu <ww433@cornell.edu>
2021-04-16 00:12:34 -0400

    evaluate setup

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,12 @@
 
-import numpy as np
+from ride_hailing.envs.ride_hailing_env import *
 
 def evaluate(model, env, numiters):
     r = 0
     r_square = 0
 
     for _ in range(numiters):
-        state = env.rest()
+        state = env.reset()
         while env.city_time < env.time_horizon:
             feasible_act = False
             # j = 0
@@ -15,4 +15,7 @@ def evaluate(model, env, numiters):
                 state, action, reward, cum_reward, feasible_act = env.step(action)
         r += env.total_reward
         r_square += env.total_reward ** 2
-    return r/numiters, (r_square - r)/numiters
+
+    mean_reward = r/numiters
+    sd_reward = np.sqrt(r_square/numiters - mean_reward ** 2)
+    return mean_reward, sd_reward

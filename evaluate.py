e2d80b483fd992bb5be417c427a8d6152c2fde71
willwu <ww433@cornell.edu>
2021-04-16 14:47:19 -0400

    evaluate setup

--- evaluate.py
+++ evaluate.py
@@ -13,7 +13,7 @@ def evaluate(model, env, numiters):
             while not feasible_act and env.city_time < env.time_horizon:
                 action = model(state, env)
                 state, action, reward, feasible_act = env.step(action)
-        print(i, numiters)
+        #print(i, numiters)
         r += env.total_reward
         r_square += env.total_reward ** 2
 
